5df682ced6d5a715ed1aa78dd34e0e37cd9e643c
Jason Zheng <jazheng@davidson.edu>
2020-06-23 14:12:39 -0400

    label appears when you hover on radar chart

--- radar.js
+++ radar.js
@@ -1,20 +1,17 @@
-let radar = document.getElementById('radarChart');
-let myRadarChart = new Chart(radar, {
+let radarData = {
+  labels: ["Attack","Defense","Health","Stamina", "XP"],
+  datasets: [
+    {
+      backgroundColor: "rgba(54, 162, 235, 0.2)",
+      data: [45, 39, 30, 35, 86],
+      label: 'Monster 1'
+    },
+  ]
+}
+
+let myRadarChart = new Chart(document.getElementById('radarChart'), {
   type: 'radar',
-  data: {
-    labels : ["Attack","Defense","Health","Stamina"],
-    datasets : [
-      {
-        fill: true,
-        backgroundColor : "rgba(54, 162, 235, 0.2)",
-        pointBackgroundColor: "blue",
-        pointBorderColor:"white",
-        borderColor : "blue",
-        label: 'Monster2',
-        data : [45,39,30,35]
-      },
-    ]
-  },
+  data: radarData,
   options: {
     scale: {
       ticks: {
@@ -25,9 +22,9 @@ let myRadarChart = new Chart(radar, {
       pointLabels: {
         fontSize: 20
       },
-        gridLines: {
-          lineWidth: 0
-       }
+      gridLines: {
+        lineWidth: 0
+      }
     }
   }
 });
